35b66780295f5ecce91dbdb30b376d11ecfd281c
Nana Vanina <vaninalarissa@gmail.com>
2023-08-24 17:50:04 +0100

    Create readme file

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # module
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # module
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
