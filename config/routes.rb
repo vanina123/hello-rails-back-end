f877c44961bcd327993aee5410705f5ca2e7f386
Nana Vanina <vaninalarissa@gmail.com>
2023-08-26 18:48:37 +0100

    Modify routes using API

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   root 'greetings#random'
-
+  namespace :api do
+    get '/greeting', to: 'greetings#random'
+  end
   resources :greetings, only: [:random], defaults: { format: 'json' } do
     collection do
       get :random
